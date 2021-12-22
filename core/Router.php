d1a2e2c3bb2f5881961c774679d62f3eef59b569
ThomasBackers <thomasbackers@gmail.com>
2021-12-22 14:58:07 +0100

    on my way through controller, commit before a break

--- core/Router.php
+++ core/Router.php
@@ -19,6 +19,11 @@ class Router
     $this->routes['get'][$path] = $callback;
   }
 
+  public function post($path, $callback)
+  {
+    $this->routes['post'][$path] = $callback;
+  }
+
   public function resolve()
   {
     $path = $this->request->getPath();
@@ -26,6 +31,7 @@ class Router
     $callback = $this->routes[$method][$path] ?? false;
     if ($callback === false) {
       $this->response->setStatusCode(404);
+      return $this->renderView('_404');
     }
     if (is_string($callback)) {
       return $this->renderView($callback);
--- views/contact.php
+++ views/contact.php
@@ -1,1 +1,21 @@
 <h1>Contact us</h1>
+
+<!-- empty action = $this (current page) -->
+<form action="" method="post">
+  <div class="mb-3">
+    <label>Subject</label>
+    <input type="text" name="subject" class="form-control">
+  </div>
+
+  <div class="mb-3">
+    <label>Email</label>
+    <input type="email" name="email" class="form-control">
+  </div>
+
+  <div class="mb-3">
+    <label>Body</label>
+    <textarea type="text" name="body" class="form-control"></textarea>
+  </div>
+
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
