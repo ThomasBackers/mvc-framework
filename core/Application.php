275af6fd76b1aaf97249a28f55b435a482a4fbf2
ThomasBackers <thomasbackers@gmail.com>
2021-12-22 14:23:34 +0100

    let's implement a controller

--- core/Application.php
+++ core/Application.php
@@ -5,13 +5,19 @@ namespace app\core;
 class Application
 {
   public static $ROOT_DIR;
-  public Request $request;
   public Router $router;
+  public Request $request;
+  public Response $response;
+  // using this
+  public static Application $app;
   public function __construct($rootPath)
   {
     self::$ROOT_DIR = $rootPath;
+    // and that, the objet is able to refer to its own instance
+    self::$app = $this;
     $this->request = new Request();
-    $this->router = new Router($this->request);
+    $this->response = new Response();
+    $this->router = new Router($this->request, $this->response);
   }
 
   public function run()
