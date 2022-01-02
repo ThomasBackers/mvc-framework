79f97cb50b8424a42046a7e5a8ae84c7d80f5845
ThomasBackers <thomasbackers@gmail.com>
2022-01-02 20:19:25 +0100

    3 hours tutorials done, let's take a break

--- migrations.php
+++ migrations.php
@@ -1,1 +1,22 @@
 <?php
+
+// is intended to be executed in the command prompt with $ php migrations.php
+require_once __DIR__.'/vendor/autoload.php';
+
+use app\core\Application;
+use Dotenv;
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$config = [
+  'db' => [
+    'dsn' => $_ENV['DB_DSN'],
+    'user' => $_ENV['DB_USER'],
+    'password' => $_ENV['DB_PASSWORD']
+  ]
+];
+
+$app = new Application(__DIR__, $config);
+
+$app->db->applyMigrations();
